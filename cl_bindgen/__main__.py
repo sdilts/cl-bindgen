0078b10effbb6daaa2cd0dd3daae46c4457560ce
sdilts <stuart.dilts@gmail.com>
2020-02-04 15:53:22 -0700

    Use util functions in __main__.py
    + Use application defaults for options instead of "wlr", "wl"

--- cl_bindgen/__main__.py
+++ cl_bindgen/__main__.py
@@ -1,37 +1,31 @@
 import sys
 
-import cl_bindgen.processfile as processfile
+from cl_bindgen.processfile import ProcessOptions
 import cl_bindgen.mangler as mangler
+import cl_bindgen.util as util
 
 def main():
-    if len(sys.argv) < 2:
-        print(f"Not enough arguments: {len(sys.argv)} given", file=sys.stderr)
-        exit(1)
-
     u_managler =  mangler.UnderscoreMangler()
     k_mangler =   mangler.KeywordMangler()
-    wlr_mangler = mangler.PrefixMangler("wlr_", "wlr:")
-    wl_mangler =  mangler.PrefixMangler("wl_", "wl:")
     const_mangler = mangler.ConstantMangler()
 
     # manglers are applied in the order that they are given in these lists:
     # enum manglers transform enum fields, e.g. FOO, BAR in enum { FOO, BAR }
     enum_manglers = [k_mangler, u_managler]
     # type mangers are applied to struct names, function names, and type names
-    type_managlers = [wl_mangler, wlr_mangler, u_managler]
+    type_managlers = [u_managler]
     # name manglers are applied to parameters and variables
     name_managlers = [u_managler]
     # typdef manglers are applied to typdefs
     typedef_manglers = [k_mangler, u_managler]
-    constant_manglers = [wl_mangler, wlr_mangler, u_managler, const_mangler]
-    processor = processfile.FileProcessor(sys.stdout,
-                                          enum_manglers=enum_manglers,
-                                          type_manglers=type_managlers,
-                                          name_manglers=name_managlers,
-                                          typedef_manglers=typedef_manglers,
-                                          constant_manglers=constant_manglers)
+    constant_manglers = [u_managler, const_mangler]
 
-    sys.exit(processor.process_file(sys.argv[1], args=sys.argv[2:1]))
+    options = ProcessOptions(typedef_manglers=type_managlers,
+                             enum_manglers=enum_manglers,
+                             type_manglers=type_managlers,
+                             name_manglers=name_managlers,
+                             constant_manglers=constant_manglers)
+    util.dispatch_from_arguments(sys.argv[1:], options)
 
 if __name__ == "__main__":
     main()
