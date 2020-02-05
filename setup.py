15182d6f1dca28d2dfaf2e420144e811cdf66dba
sdilts <stuart.dilts@gmail.com>
2020-02-04 22:05:33 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,20 @@ requirements = [
     'PyYAML'
 ]
 
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    proj_license = f.read()
+
 setup(name="cl_bindgen",
       version='0.1.0',
+      description='command line tool and library for creating common lisp language bindings from C header files',
+      long_description=readme,
+      license=proj_license,
+      author="Stuart Dilts",
+      author_email='stuart.dilts@gmail.com',
+      url='https://github.com/sdilts/cl-bindgen',
       packages=['cl_bindgen'],
       entry_points={
           'console_scripts': [
