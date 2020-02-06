6f24b537ed1cc3466f3958844741e8b0212e1422
sdilts <stuart.dilts@gmail.com>
2020-02-05 22:50:49 -0700

    Bump version number

--- cl_bindgen/util.py
+++ cl_bindgen/util.py
@@ -88,7 +88,7 @@ def _build_parser():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--version',action='version',
-                        version='CL-BINDGEN 1.0.2',
+                        version='CL-BINDGEN 1.0.3',
                         help="Print the version information")
     subparsers = parser.add_subparsers()
 
