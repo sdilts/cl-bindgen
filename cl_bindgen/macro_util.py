a8bcb6a6fd5cef59a69ef323c87d06399926ecd4
Stuart Dilts <stuart.dilts@gmail.com>
2023-10-03 20:39:45 -0600

    Add missing macro_util.py file

---
+++ cl_bindgen/macro_util.py
@@ -0,0 +1,26 @@
+import re
+import sys
+from pathlib import Path
+
+def macro_matches_file_path(location: str, name: str):
+    location_path = Path(location)
+    parent_name = '_'.join([g.name for g in location_path.parents]) + name.replace('.', '_')
+    return parent_name.upper().endswith(name.upper())
+
+def is_header_guard(cursor, detector_fn):
+    if not detector_fn:
+        return False
+
+    location = cursor.location
+    macro_name = cursor.spelling
+    return detector_fn(location.file.name, macro_name)
+
+
+class LiteralConversionError(Exception):
+    pass
+
+def convert_literal_token(token):
+    if re.fullmatch('"(.*)"|([0-9]*\.{0,1}[0-9]*)', token.spelling):
+        return token.spelling
+    else:
+        raise LiteralConversionError()
